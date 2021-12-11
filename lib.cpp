1e910bb430bba7a79b9cb6265e7a9f80628c2772
matteomusso07 <91545034+matteomusso07@users.noreply.github.com>
2021-12-11 18:36:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+//
+// Created by 39339 on 11/12/2021.
+//
+#include "lib.h"
+float numeromassimo(const float numeri[], int dim){
+    numeri [dim];
+    float massimo= numeri[0];
+    for (int i=0; i<dim; i++){
+        if(massimo < numeri[i]){
+            massimo= numeri[i];
+        }
+    }
+    return massimo;
+}
